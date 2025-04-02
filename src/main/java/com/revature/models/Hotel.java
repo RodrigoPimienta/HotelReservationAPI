2721bb4126a25c3f11a5896c6f8a01db75049082
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-04-01 21:42:55 -0600

    feat(models): add model layer
    add the following models:
    - Hotel
    - HotelImage
    - HotelsAmenity
    
    These models are used to represent hotel data and its associated images and amenities. HotelImage has a foreign key relationship with Hotel.

---
+++ src/main/java/com/revature/models/Hotel.java
@@ -0,0 +1,139 @@
+package com.revature.models;
+
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int hotelId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 30)
+    private String Country;
+
+    @Column(nullable = false, length = 30)
+    private String State;
+
+    @Column(nullable = false, length = 30)
+    private String City;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false, length = 10)
+    private String houseNumber;
+
+    @Column(nullable = false, length = 10)
+    private String postalCode;
+
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HotelAmenity> amenities;
+
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HotelImage> images;
+
+    public Hotel() {
+
+    }
+
+    public Hotel(int hotelId, String name, String country, String state, String city, String street, String houseNumber, String postalCode, List<HotelAmenity> amenities, List<HotelImage> images) {
+        this.hotelId = hotelId;
+        this.name = name;
+        Country = country;
+        State = state;
+        City = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.postalCode = postalCode;
+        this.amenities = amenities;
+        this.images = images;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public List<HotelAmenity> getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(List<HotelAmenity> amenities) {
+        this.amenities = amenities;
+    }
+
+    public List<HotelImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<HotelImage> images) {
+        this.images = images;
+    }
+}
---
+++ src/main/java/com/revature/models/HotelAmenity.java
@@ -0,0 +1,80 @@
+package com.revature.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hotel_amenities")
+public class HotelAmenity {
+
+//    amenity_id serial primary key,
+//    hotel_id int references hotels,
+//    name varchar(40) not null,
+//    description varchar(255) not null
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int hotelAmenityId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    public HotelAmenity(){}
+
+    public HotelAmenity(int hotelAmenityId, String name, String description, String url, Hotel hotel) {
+        this.hotelAmenityId = hotelAmenityId;
+        this.name = name;
+        this.description = description;
+        this.url=url;
+        this.hotel = hotel;
+    }
+
+    public int getHotelAmenityId() {
+        return hotelAmenityId;
+    }
+
+    public void setHotelAmenityId(int hotelAmenityId) {
+        this.hotelAmenityId = hotelAmenityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+}
