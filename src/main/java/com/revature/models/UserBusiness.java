cce584be460dae780b44d8aca5581fbacccd67a2
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-04-02 22:56:04 -0600

    feat(models): add user models
    add new models with a OneToOne relation
    
    -User
    -UserBusiness

---
+++ src/main/java/com/revature/models/UserBusiness.java
@@ -0,0 +1,77 @@
+package com.revature.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_businesses")
+public class UserBusiness {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int businessId;
+
+    @Column(nullable = false)
+    private String legalName;
+
+    @Column(nullable = false, length = 13)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 20)
+    private String taxCode;
+
+    @JsonBackReference
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public UserBusiness(){}
+
+    public UserBusiness(int businessId, String legalName, String phoneNumber, String taxCode) {
+        this.businessId = businessId;
+        this.legalName = legalName;
+        this.phoneNumber = phoneNumber;
+        this.taxCode = taxCode;
+    }
+
+    public int getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(int businessId) {
+        this.businessId = businessId;
+    }
+
+    public String getLegalName() {
+        return legalName;
+    }
+
+    public void setLegalName(String legalName) {
+        this.legalName = legalName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getTaxCode() {
+        return taxCode;
+    }
+
+    public void setTaxCode(String taxCode) {
+        this.taxCode = taxCode;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
